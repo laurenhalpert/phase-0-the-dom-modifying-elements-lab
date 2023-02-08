c29e4e7e05f78ce696fb8e7f1f731419f900e022
Lauren <halpertln@gmail.com>
2023-02-08 16:16:32 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector("#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+const body = document.getElementsByTagName("body");
+
+newHeader.id = "victory";
+newHeader.textContent = "Lauren is the champion";
+document.body.append(newHeader);
+
+
